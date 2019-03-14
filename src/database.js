448363a5dfb19cca044f1531719c2e83f3abb6ee
Yair Tavizon <33972438+YairTavizon@users.noreply.github.com>
2019-03-14 02:33:47 -0600

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose');
-const URI = 'mongodb://YairTav:hola1234@ds119598.mlab.com:19598/people';
+const URI = 'your connection string;
 
 mongoose.connect(URI)
   .then(db => console.log('Db is connected'))
